4a151b6d375a93920eb6242b95406840ecd88d3d
Helder Filipe <hfeveris@gmail.com>
2020-01-17 18:11:34 +0000

    Prototype Pattern Created

---
+++ desigPatterns/src/designPatterns/prototype/Ford.java
@@ -0,0 +1,14 @@
+package designPatterns.prototype;
+
+public class Ford extends BasicCar {
+
+	public Ford(String m) {
+		modelname = m;
+	}
+
+	@Override
+	public BasicCar clone() throws CloneNotSupportedException {
+		return (Ford) super.clone();
+	}
+
+}
