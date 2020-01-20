c67399ae5552e804077e60291f2c7732dd025bce
Helder Filipe <hfeveris@gmail.com>
2020-01-20 18:14:38 +0000

    Template Pattern Created

---
+++ desigPatterns/src/designPatterns/behavioralPatterns/template/ComputerScience.java
@@ -0,0 +1,10 @@
+package designPatterns.behavioralPatterns.template;
+
+public class ComputerScience extends BasicEngineering {
+
+	@Override
+	public void SpecialPaper() {
+		System.out.println("Object Oriented Programming");
+	}
+
+}
---
+++ desigPatterns/src/designPatterns/behavioralPatterns/template/TestTemplate.java
@@ -0,0 +1,25 @@
+package designPatterns.behavioralPatterns.template;
+
+public class TestTemplate {
+
+	public static void main(String[] args) {
+
+		System.out.println("***Template Method Pattern Demo***\n");
+
+		BasicEngineering bs = new ComputerScience();
+
+		System.out.println("Computer Sc Papers:");
+
+		bs.Papers();
+
+		System.out.println();
+
+		bs = new Electronics();
+
+		System.out.println("Electronics Papers:");
+
+		bs.Papers();
+
+	}
+
+}
