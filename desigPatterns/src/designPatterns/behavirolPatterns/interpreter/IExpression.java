4dac26dc77d5967e87d7c1c307f65050fe4c6349
Helder Filipe <hfeveris@gmail.com>
2020-01-20 16:20:19 +0000

    Interpreter Pattern Created

---
+++ desigPatterns/src/designPatterns/behavirolPatterns/interpreter/IExpression.java
@@ -0,0 +1,7 @@
+package designPatterns.behavirolPatterns.interpreter;
+
+public interface IExpression {
+	
+	void interpret(Context ic);
+
+}
---
+++ desigPatterns/src/designPatterns/behavirolPatterns/interpreter/TestInterpreter.java
@@ -0,0 +1,48 @@
+package designPatterns.behavirolPatterns.interpreter;
+
+import java.util.Scanner;
+
+public class TestInterpreter {
+
+	public Context clientContext = null;
+	public IExpression exp = null;
+
+	public TestInterpreter(Context c) {
+
+		clientContext = c;
+	}
+
+	public void interpret(String str) {
+		// We'll test 2 consecutive inputs at a time
+		for (int i = 0; i < 2; i++) {
+
+			System.out.println("\nEnter ur choice(1 or 2)");
+			Scanner in = new Scanner(System.in);
+			String c = in.nextLine();
+
+			if (c.equals("1")) {
+				exp = new IntToWords(str);
+				exp.interpret(clientContext);
+			} else {
+				exp = new StringToBinaryExp(str);
+				exp.interpret(clientContext);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("\n***Interpreter Pattern Demo***\n");
+		System.out.println("Enter a number :");
+
+		Scanner in = new Scanner(System.in);
+		String input = in.nextLine();
+
+		Context context = new Context(input);
+		TestInterpreter client = new TestInterpreter(context);
+
+		client.interpret(input);
+
+	}
+
+}
