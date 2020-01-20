28b74bafe622c7f44e958b49265513458d8c3357
Helder Filipe <hfeveris@gmail.com>
2020-01-20 17:25:34 +0000

    Memento Pattern Created

---
+++ desigPatterns/src/designPatterns/behavioralPatterns/memento/Caretaker.java
@@ -0,0 +1,15 @@
+package designPatterns.behavioralPatterns.memento;
+
+public class Caretaker {
+
+	private Memento _memento;
+
+	public void SaveMemento(Memento m) {
+		_memento = m;
+	}
+
+	public Memento RetrieveMemento() {
+		return _memento;
+	}
+
+}
---
+++ desigPatterns/src/designPatterns/behavioralPatterns/memento/Memento.java
@@ -0,0 +1,15 @@
+package designPatterns.behavioralPatterns.memento;
+
+public class Memento {
+
+	private String state;
+
+	public Memento(String state) {
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+}
